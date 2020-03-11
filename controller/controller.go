9fe3022d5b693ea2116874f9ec65b1829c45e96c
Mehmed <mehmedadzemovic@gmail.com>
2020-03-11 19:47:51 +0100

    added ""pseudocode""

--- controller/controller.go
+++ controller/controller.go
@@ -9,7 +9,7 @@ import (
   "time"
 )
 
-type Drv struct {
+type Event struct {
   buttons chan elevio.ButtonEvent
   floors chan int
   obstr chan bool
@@ -21,36 +21,36 @@ type Order_com struct {
   order_done chan structs.Order
 }
 
-func Init_elev(port int, numFloors int) (Drv, elevio.MotorDirection, int) {
+func Init_elev(port int, numFloors int) (Event, elevio.MotorDirection, int) {
   elevio.Init("localhost:"+strconv.Itoa(port), numFloors)
 
   var d elevio.MotorDirection = elevio.MD_Down
   floor := -1
 
-  drv := Drv{
+  event := Event{
     buttons:    make(chan elevio.ButtonEvent),
     floors:     make(chan int),
     obstr:      make(chan bool),
     stop:       make(chan bool)}
 
-  go elevio.PollButtons(drv.buttons)
-  go elevio.PollFloorSensor(drv.floors)
-  go elevio.PollObstructionSwitch(drv.obstr)
-  go elevio.PollStopButton(drv.stop)
+  go elevio.PollButtons(event.buttons)
+  go elevio.PollFloorSensor(event.floors)
+  go elevio.PollObstructionSwitch(event.obstr)
+  go elevio.PollStopButton(event.stop)
 
   //go to known state (down to closest floor)
   elevio.SetMotorDirection(d)
-  floor = <-drv.floors
+  floor = <-event.floors
   d = elevio.MD_Stop
   elevio.SetMotorDirection(d)
 
-  return drv, d, floor
+  return event, d, floor
 }
 
-func Fake_gen_orders(orders Order_com, drv Drv) {
+func Fake_gen_orders(orders Order_com, event Event) {
   for {
     select {
-    case a := <- drv.buttons:
+    case a := <- event.buttons:
       orders.order <- structs.Order{Floor: a.Floor,
                         Button: a.Button}
     case b := <-orders.order_done:
@@ -59,7 +59,28 @@ func Fake_gen_orders(orders Order_com, drv Drv) {
   }
 }
 
-func Operate_elev(orders Order_com, drv Drv, d elevio.MotorDirection, f int) {
+func Operate_elev(orders Order_com, event Event, d elevio.MotorDirection, f int) {
+  /*pseudocode:
+  create 2d-array for orders.
+  if orders in current dir: go to the one furthest away
+  elseif orders in other dir: choose furthest
+  else stop
+
+  if driving by floor
+  check for orders: open/close door, confirm order
+
+  elevator should somehow give its status
+  can be done when passing floors
+  motor change should also be updated
+  both sent on status channel, not sure if this needs its own module
+  (harder to implement separate module, but doing it every floor/motor change gives
+  irregular updates. Possibly need timestamp, given by status sender!)
+
+  one routine should send button presses to order module
+  should also recieve orders and update the local array (with mutex)
+  this allows the elevator to be "stupid", only needs to check stuff when it hits a floor
+
+  */
   current_floor := f
   floor_goal := -1
   for {
@@ -72,7 +93,7 @@ func Operate_elev(orders Order_com, drv Drv, d elevio.MotorDirection, f int) {
       }
       floor_goal = a.Floor
 
-    case b := <-drv.floors:
+    case b := <-event.floors:
       current_floor = b
       if current_floor == floor_goal {
         elevio.SetMotorDirection(elevio.MD_Stop)
@@ -87,14 +108,15 @@ func Operate_elev(orders Order_com, drv Drv, d elevio.MotorDirection, f int) {
 
 
 func main(){
-  drv, d, f := Init_elev(15657, 4)
+  event, dir, floor := Init_elev(15657, 4)
 
-  o := Order_com{
+  //this should be made in the real main and sent to the order module as well
+  orders := Order_com{
     order: make(chan structs.Order),
     order_done: make(chan structs.Order)}
 
-  go Fake_gen_orders(o, drv)
-  go Operate_elev(o, drv, d, f)
+  go Fake_gen_orders(orders, event)
+  go Operate_elev(orders, event, dir, floor)
 
   for{}
 }
