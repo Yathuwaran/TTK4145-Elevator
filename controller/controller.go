23278defac3db1c103f4cf154b6dde194756aaad
Mehmed <mehmedadzemovic@gmail.com>
2020-04-09 16:55:53 +0200

    f

--- controller/controller.go
+++ controller/controller.go
@@ -254,36 +254,3 @@ func Operate_elev(orders structs.Order_com, event Event, f int, maxFloors int) {
 		}
 	}
 }
-
-
-func main(){
-  event, current_floor := Init_elev(15657, 4)
-
-  //this should be made in the real main and sent to the order module as well
-  orders := structs.Order_com{
-    OrderFromButton: make(chan structs.Order, 4096),
-		OrderForLocal: make(chan structs.Order, 4096),
-    OrderDone: make(chan structs.Order, 4096),
-	  Light: make(chan structs.LightOrder, 4096)}
-
-  numFloors := 4
-  go Fake_gen_orders(orders, event)
-	fmt.Println("Initiated fake orders")
-  go Operate_elev(orders, event, current_floor, numFloors)
-  fmt.Println("Initiated operate elev")
-  for{}
-}
-
-
-/*
-func Elev_status() {}
-
-
-func motor_move(dir) {}
-func motor_stop() {}
-func motor_status() {}
-
-func light_on(light_num) {}
-func light_off(light_num) {}
-func check_light_status(light_num) {}
-*/
