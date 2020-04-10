694bd6b4bcb2a7eaf5ecd36848ba95757750dfc5
Mehmed <mehmedadzemovic@gmail.com>
2020-04-10 15:03:58 +0200

    fixed controller 4 real

--- controller/controller.go
+++ controller/controller.go
@@ -3,8 +3,6 @@ package controller
 
 import (
 	"fmt"
-  //"strconv"
-
 	"../elevio"
   "../structs"
   "time"
@@ -118,13 +116,55 @@ func OrdersInDirection(dir int, localOrders [][]int, currentFloor int, maxFloors
 }
 
 
+func updateMovement(lastDir *int, localOrders [][]int, currentFloor int, maxFloors int, idle *bool, Update_out_msg_CH chan<- structs.Message_struct, outgoing_msg structs.Message_struct) {
+	fmt.Printf("Updating movement \n")
+	oppositeDir := *lastDir * (-1)
+	if (OrdersInDirection(*lastDir, localOrders, currentFloor, maxFloors)) {
+		elevio.SetMotorDirection(elevio.MotorDirection(*lastDir))
+		outgoing_msg.Dir = structs.MotorDirection(*lastDir)
+		outgoing_msg.State = 1
+		*idle = false
+
+	} else if (OrdersInDirection(oppositeDir, localOrders, currentFloor, maxFloors)) {
+		elevio.SetMotorDirection(elevio.MotorDirection(oppositeDir))
+		*lastDir = oppositeDir
+		outgoing_msg.Dir = structs.MotorDirection(oppositeDir)
+		outgoing_msg.State = 1
+		*idle = false
+
+	} else {
+		elevio.SetMotorDirection(elevio.MD_Stop)
+		outgoing_msg.Dir = 0
+		outgoing_msg.State = 0
+		*idle = true
+	}
+	go func() { Update_out_msg_CH <- outgoing_msg }()
+	fmt.Printf("Updated movement \n")
+}
+
+//always updateMovement after executeStop
+func executeStop(localOrders [][]int, orders structs.Order_com, currentFloor int, Update_out_msg_CH chan<- structs.Message_struct, outgoing_msg structs.Message_struct) {
+	fmt.Printf("Executing stop at floor %d\n", currentFloor)
+	elevio.SetMotorDirection(elevio.MD_Stop)
+	elevio.SetDoorOpenLamp(true)
+	outgoing_msg.State = 2
+	time.Sleep(1 * time.Second)
+	elevio.SetDoorOpenLamp(false)
+	go func() { Update_out_msg_CH <- outgoing_msg }()
+	localOrders[currentFloor][0] = 0
+	localOrders[currentFloor][1] = 0
+	localOrders[currentFloor][2] = 0
+	orders.OrderDone <- structs.Order{Floor: currentFloor}
+	fmt.Printf("Done executing floor %d\n", currentFloor)
+}
+
+
 func Operate_elev(orders structs.Order_com, event Event, f int, maxFloors int, Update_out_msg_CH chan<- structs.Message_struct, outgoing_msg structs.Message_struct) {
 
   go UpdateLights(orders)
 	go SendButtonPresses(orders, event)
 
-	//localOrders[floor][button]
-	//buttons 0 to 2: up,down,cab
+	//localOrders[floor][button], buttons 0 to 2: up, down, cab
 	localOrders := make([][]int, maxFloors)
   for i := 0; i < maxFloors; i++ {
 		localOrders[i] = make([]int, 3)
@@ -133,23 +173,23 @@ func Operate_elev(orders structs.Order_com, event Event, f int, maxFloors int, U
   currentFloor := f
 	lastDir := elevio.MD_Down
 
-  updateMovement := make(chan int, 4096)
-  executeStop := make(chan int, 4096)
+  //updateMovement := make(chan int, 4096)
+  //executeStop := make(chan int, 4096)
 	idle := true
 
   for {
 		select {
 		case order := <- orders.OrderForLocal:
 		  localOrders[order.Floor][order.Button] = 1
-			updateMovement <- 1
+			updateMovement(&lastDir, localOrders, currentFloor, maxFloors, &idle, Update_out_msg_CH, outgoing_msg)
 			fmt.Println(localOrders)
 			outgoing_msg.Queue = localOrders
 			go func() { Update_out_msg_CH <- outgoing_msg }()
 			if (idle == true && order.Floor == currentFloor) {
-				executeStop <- 1
-				//executeStop(localOrders, orders, currentFloor)
+				//executeStop <- 1
+				executeStop(localOrders, orders, currentFloor, Update_out_msg_CH, outgoing_msg)
 			}
-			//updateMovement(lastDir, localOrders, currentFloor, maxFloors, &idle)
+			updateMovement(&lastDir, localOrders, currentFloor, maxFloors, &idle, Update_out_msg_CH, outgoing_msg)
 
 		case floor := <-event.floors:
         outgoing_msg.Last_floor = floor
@@ -157,19 +197,15 @@ func Operate_elev(orders structs.Order_com, event Event, f int, maxFloors int, U
         currentFloor = floor
 				elevio.SetFloorIndicator(floor)
         if ShouldStop(localOrders, currentFloor, lastDir, maxFloors) {
-					executeStop <- 1
-					//executeStop(localOrders, orders, currentFloor)
-					//updateMovement(lastDir, localOrders, currentFloor, maxFloors, &idle)
+					//executeStop <- 1
+					executeStop(localOrders, orders, currentFloor, Update_out_msg_CH, outgoing_msg)
+					updateMovement(&lastDir, localOrders, currentFloor, maxFloors, &idle, Update_out_msg_CH, outgoing_msg)
 				} else {
-					updateMovement <- 1
-					//updateMovement(lastDir, localOrders, currentFloor, maxFloors, &idle)
+					//updateMovement <- 1
+					updateMovement(&lastDir, localOrders, currentFloor, maxFloors, &idle, Update_out_msg_CH, outgoing_msg)
 				}
-
+/*
 		case <-updateMovement:
-		  //check for orders in current dir
-			//(check for orders in other dir)
-			//go towards order
-			//or idle
 			fmt.Printf("Updating movement \n")
 			oppositeDir := lastDir * (-1)
 			if (OrdersInDirection(lastDir, localOrders, currentFloor, maxFloors)) {
@@ -209,7 +245,7 @@ func Operate_elev(orders structs.Order_com, event Event, f int, maxFloors int, U
 			orders.OrderDone <- structs.Order{Floor: currentFloor}
 			updateMovement <- 1
 			fmt.Printf("Done executing floor %d\n", currentFloor)
-
+*/
 		}
 	}
 }
